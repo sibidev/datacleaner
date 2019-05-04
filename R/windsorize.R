8cd6a4108f8db32f2a9d026c71b4dedd45ef1d2e
[patrick sibetz] <[sibetz.patrick@gmail.com]>
2019-05-04 10:45:34 +0200

    Fixed windsorize issues: * wrong implementation #1 * lacking wrong input handling #2

--- R/windsorize.R
+++ R/windsorize.R
@@ -1,10 +1,26 @@
-#' Windsorize
+#' windsorize
 #'
-#' Do some windsorization.
+#' see https://en.wikipedia.org/wiki/Winsorizing
+#'
+#' @param x A vector
+#' @param p A quantile
+#' 
+#' @examples
+#' windsorize(rnorm(7))
+#' 
 #' @export
 windsorize <- function(x, p = .90) {
-  q <- quantile(x, p)
-  x[x >= q] <- q
+  
+  # value checks
+  if( length(x)==0 ) stop("Input must not be empty!") 
+  if( all(is.na(x)) ) stop("Input must not only contain NAs!")
+
+  q_upper <- quantile(x, 1-(1-p)/2 )
+  q_lower <- quantile(x, (1-p)/2 )
+  x[x >= q_upper] <- q_upper
+  x[x <= q_lower] <- q_lower
   x
+  
 }
+
 
